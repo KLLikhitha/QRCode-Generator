eb807fcfcf6306db55a01b51d93a935b61907fa9
KLLikhitha <111566540+KLLikhitha@users.noreply.github.com>
2024-04-08 16:10:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.btn');
+const code = document.querySelector('.code');
+const input = document.querySelector('.input');
+const toast = document.querySelector('#toast');
+
+btn.addEventListener('click', generate);
+
+function generate() {
+	const data = input.value.trim();
+    
+    // Check if input is empty
+    if (data === '') {
+        // Display error message
+        alert('Please enter text before generating.');
+        return; // Exit the function without generating QR code
+    }
+	const URL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${data}`;
+	code.src = URL;
+
+	toastDiv();
+}
+
+function toastDiv() {
+	toast.className = "show";
+	setTimeout(function() {
+		toast.className = toast.className.replace("show", "");
+	}, 2000)
+}
